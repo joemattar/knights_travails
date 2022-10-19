ec450c70ca2f26ff75f70d9330161ffb6f8e0b51
Joe M <joemattar@gmail.com>
2022-10-19 18:19:18 -0400

    Create body main sub elements

--- src/index.js
+++ src/index.js
@@ -1,5 +1,41 @@
-import "./styles.css";
+import "./css/styles.css";
+import githubImage from "./images/github_logo.png";
 
-const divy = document.createElement("div");
-divy.textContent = "TEST";
-document.body.appendChild(divy);
+// Create header element
+const header = document.createElement("header");
+header.classList.add("header");
+header.textContent = "Knights Travails";
+document.body.appendChild(header);
+
+// Create chessboard div element
+const chessboardDiv = document.createElement("div");
+chessboardDiv.classList.add("chessboard");
+document.body.appendChild(chessboardDiv);
+
+// Create instructions div element
+const instructionsDiv = document.createElement("div");
+instructionsDiv.classList.add("instructions");
+instructionsDiv.textContent =
+  "Use the mouse to drag the knights to their starting and target positions";
+document.body.appendChild(instructionsDiv);
+
+// Create footer element
+const footer = document.createElement("footer");
+footer.classList.add("footer");
+document.body.appendChild(footer);
+
+// Footer text element
+const footerTextDiv = document.createElement("div");
+footerTextDiv.textContent = "The Odin Project - joemattar";
+footer.appendChild(footerTextDiv);
+
+// Footer a element
+const footerA = document.createElement("a");
+footerA.href = "https://github.com/joemattar";
+footerA.target = "_blank";
+footer.appendChild(footerA);
+
+// Footer img element
+const footerImg = document.createElement("img");
+footerImg.src = githubImage;
+footerA.appendChild(footerImg);
